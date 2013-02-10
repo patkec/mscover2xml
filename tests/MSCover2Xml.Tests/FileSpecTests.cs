b2589eeec94b0fded64f3c3cb1b6753088fc7ffa
patkec <patko@outlook.com>
2013-02-10 19:48:12 +0100

    Removed dummy method.

--- tests/MSCover2Xml.Tests/FileSpecTests.cs
+++ tests/MSCover2Xml.Tests/FileSpecTests.cs
@@ -18,16 +18,5 @@ namespace MSCover2Xml.Tests
             // Assert
             xml.Should().Be("<Root><FileID>1</FileID><FileName>file.ext</FileName></Root>");
         }
-
-        /*[TestMethod]
-        public void Test()
-        {
-            var input =
-                @"d:\Projects\_MSCover2Xml\TestResults\06373912-c0f7-4bf4-97de-6f855012daf2\Simon_SIMON-PC 2012-10-21 22_20_26.coverage";
-            var path = new[] {@"d:\Projects\_MSCover2Xml\TestResults\Simon_SIMON-PC 2012-06-27 23_23_06\Out\"};
-            var report = CoverageReportGenerator.Create(input, path, path);
-
-            report.Should().NotBeNull();
-        }*/
     }
 }
